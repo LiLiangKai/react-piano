36ba6357151d9151d625cc7169a632e44e66edc4
LiLiangKai <1147036010@qq.com>
2019-09-26 09:50:35 +0800

    build

---
+++ build/precache-manifest.08a290f8e9c72cc5715a3e0d195d4a33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2018de4ee00842d02cb11d7857697669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e012cd20609bae9a2efc",
+    "url": "/static/css/main.37597655.chunk.css"
+  },
+  {
+    "revision": "1a1c7d87dd6d7f2c822d",
+    "url": "/static/js/2.6c5e3517.chunk.js"
+  },
+  {
+    "revision": "e012cd20609bae9a2efc",
+    "url": "/static/js/main.0b945fc3.chunk.js"
+  },
+  {
+    "revision": "72071f09f269d5360e3a",
+    "url": "/static/js/runtime~main.ab7b96b8.js"
+  }
+]);
